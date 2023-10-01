909769c77b09b8b6154a97fe97c69b02ca0c1519
yesaroun <akor1@naver.com>
2023-10-01 11:09:59 +0900

    [feat] is_liked 구현

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
+from wishlists.models import Wishlist
 from rooms.models import Amenity, Room
+from medias.serializers import PhotoSerializer
 from users.serializers import TinyUserSerializer
 from categories.serializers import CategorySerializer
-from medias.serializers import PhotoSerializer
 
 
 class AmenitySerializer(serializers.ModelSerializer):
@@ -19,6 +20,7 @@ class RoomDetailSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     rating = serializers.SerializerMethodField()
     is_owner = serializers.SerializerMethodField()
+    is_liked = serializers.SerializerMethodField()
     photos = PhotoSerializer(many=True, read_only=True)
 
     class Meta:
@@ -31,6 +33,13 @@ class RoomDetailSerializer(serializers.ModelSerializer):
     def get_is_owner(self, room):
         request = self.context["request"]
         return room.owner == request.user
+
+    def get_is_liked(self, room):
+        request = self.context["request"]
+        return Wishlist.objects.filter(
+            user=request.user,
+            rooms__pk=room.pk,
+        ).exists()
 
 
 class RoomListSerializer(serializers.ModelSerializer):
